6a7cdc1e3dca6c6000fb0700658730c83ed0ad73
hzr <1033387948@qq.com>
2024-07-10 08:29:38 +0000

    lab2 wrap/unwrap完成

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,23 @@
 #include "wrapping_integers.hh"
+#include <math.h>
+#include <iostream>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>(n);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t max2_32 = (uint64_t)1 << 32;
+  uint64_t n = checkpoint / max2_32;
+  uint64_t offset = raw_value_ - zero_point.raw_value_;
+  checkpoint %= max2_32;
+  if((offset < checkpoint) && (checkpoint - offset > max2_32/2))
+    return (n+1) * max2_32 + offset;
+  else if((offset > checkpoint) && (offset - checkpoint > max2_32/2) &&( n >0))
+    return (n-1) * max2_32 + offset;
+  return n * max2_32 + offset;
 }
